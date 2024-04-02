318bf8a18633bf16669e028b460397f885de3cf6
1etterh <1etterh.dev@gmail.com>
2024-04-02 15:31:22 +0900

    edit
    .env
    - RDS configurations
    
    database.js
    - create connections
    
    install:
    mysql

---
+++ Readme.md
@@ -0,0 +1,4 @@
+### Repo: side project for studying 
+1. Vue.js
+2. Node.js
+3. AWS RDS for MySQL
---
+++ backend/database.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const mysql = require('mysql')
+
+const connection = mysql.createConnection({
+    host     : process.env.RDS_HOSTNAME,
+    user     : process.env.RDS_USERNAME,
+    password : process.env.RDS_PASSWORD,
+    port     : process.env.RDS_PORT
+  });
+
+connection.connect((err)=>{
+    if(err) throw err;
+    console.log('connected to rds for mysql')
+})
+
