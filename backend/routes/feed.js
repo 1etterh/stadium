05ab3076ec143c6d9fee09e31749e634291c8e7a
1etterh <1etterh.dev@gmail.com>
2024-04-03 17:54:23 +0900

    edit: feed.js
    -edit: feed.js
    edit query to drop unneccessary infos

--- backend/routes/feed.js
+++ backend/routes/feed.js
@@ -5,7 +5,7 @@ const connection= require('../database')
 // const userdata=require('./../userdata.json')
 /* GET feed  */
 // connection.connect();
-const joinQuery = 'SELECT * FROM credential.user t1 Join post.post t2 on t2.userId=t1.userId'
+const joinQuery = 'SELECT username, profileImage, contents, image, datetime FROM credential.user t1 Join post.post t2 on t2.userId=t1.userId'
 router.get('/',async(req, res, next)=>{
   console.log('/routes/feed.js')
   await connection.query(joinQuery,(err,rows,fields)=>{
