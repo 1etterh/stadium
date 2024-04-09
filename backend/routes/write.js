dd49fdae748c711b2b2924d422793bf6a77de116
1etterh <1etterh.dev@gmail.com>
2024-04-09 13:26:48 +0900

    test: uploadimage

--- backend/routes/write.js
+++ backend/routes/write.js
@@ -1,55 +1,78 @@
 const express = require('express');
-const { S3Client, ListObjectsCommand } = require("@aws-sdk/client-s3");
-const multer = require('multer');
-const multerS3 = require('multer-s3');
+const { S3Client, ListObjectsCommand,PutObjectCommand } = require("@aws-sdk/client-s3");
 require('dotenv').config();
 const router = express.Router();
 const isLoggedIn = require('./../middleware/isAuthenticated');
 const connection = require('../database');
 const path=require("path")
+const https= require('https')
+const fromIni=require('@aws-sdk/credential-providers');
+const HttpRequest=require('@smithy/protocol-http')
+const {getSignedUrl, S3RequestPresigner} = require("@aws-sdk/s3-request-presigner")
+const parseUrl=require('@smithy/url-parser')
+const formatUrl = require('@aws-sdk/util-format-url')
+const Hash = require('@smithy/hash-node')
 // Create S3 client
-const s3Client = new S3Client({
-  region: process.env.S3_AWS_REGION,
-  credentials: {
-    accessKeyId: process.env.S3_ACCESS_KEY,
-    secretAccessKey: process.env.S3_SECRET_KEY
-  }
-});
 
+const createPresignedUrlWithClient = ({ region, bucket, key }) => {
+  const client = new S3Client({ region });
+  const command = new PutObjectCommand({ Bucket: bucket, Key: key });
+  return getSignedUrl(client, command, { expiresIn: 3600 });
+};
 
-
+function put(url, data) {
+  return new Promise((resolve, reject) => {
+    const req = https.request(
+      url,
+      { method: "PUT", headers: { "Content-Length": new Blob([data]).size } },
+      (res) => {
+        let responseBody = "";
+        res.on("data", (chunk) => {
+          responseBody += chunk;
+        });
+        res.on("end", () => {
+          resolve(responseBody);
+        });
+      },
+    );
+    req.on("error", (err) => {
+      reject(err);
+    });
+    req.write(data);
+    req.end();
+  });
+}
+// const listObjectsParams = { Bucket: process.env.S3_BUCKET_NAME };
+// s3Client.send(new ListObjectsCommand(listObjectsParams))
+//   .then(data => {
+//     console.log("List of objects in the bucket:", data.Contents);
+//   })
+//   .catch(error => {
+//     console.error("Error listing objects in the bucket:", error);
+//   });
 
 // Multer middleware for file upload to S3
-const upload = multer({
-  storage:multer.diskStorage({
-    destination:function(req,file,done){
-      done(null,'uploads/')
-    },
-    filename:function(req,file,done){
-    
-      const fileName=Date.now()+file.originalname;
-      done(null,fileName)
-    }
-  })
-});
 
 
-if(s3Client){
-  console.log('s3 Connected')
-}
+
+
 // Middleware to check if user is authenticated
 router.use(isLoggedIn);
-
 // Route to publish a post with image upload to S3
-router.post('/',upload.single('image'),async (req, res) => {
+router.post('/',async (req, res) => {
   console.log(req.file)
+  const clientUrl = await createPresignedUrlWithClient({
+    region : process.env.S3_AWS_REGION,
+    bucket : process.env.S3_BUCKET_NAME,
+    key: req.file.originalname
+  })
   try {
-    if (!req.file) {
-      return res.status(400).json({ message: 'No file uploaded' });
-    }
+    // if (!req.file) {
+    //   return res.status(400).json({ message: 'No file uploaded' });
+    // }
 
     // Access the uploaded image URL in S3
-    const imageUrl = req.file.location;
+    const imageUrl = clientUrl;
     // Access the caption using req.body.caption
     const contents = req.body.contents;
     // Retrieve userId from the session
