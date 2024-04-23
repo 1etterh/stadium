a2606c251a07e9ad31213c591f09e4b4dfcd29a4
1etterh <1etterh.dev@gmail.com>
2024-04-23 16:54:44 +0900

    edit: move statusBar, tabBar to App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,34 +1,40 @@
 <template>
+  <div class="app-container">
+  <div class="status-bar">
+      <!-- <router-link to="/login">Login</router-link> -->
+      <span><font-awesome-icon :icon="['fa', 'paper-plane']" /></span>
+    </div>
 
-    <router-view></router-view>
+    <div class="router-container">
+      <router-view></router-view>
+    </div>
+    <div class="tab-bar">
+      <span><font-awesome-icon :icon="['fa', 'home']"/></span>
+      <span><font-awesome-icon :icon="['fa', 'search']"/></span>
+      <router-link to="/write"><span><font-awesome-icon :icon="['fa', 'plus-square']" /></span> </router-link>
+      <span><font-awesome-icon :icon="['fa', 'heart']" /></span>
+      <span><font-awesome-icon :icon="['fa', 'user-circle']" /></span>
+    </div>
 
+  </div>
 </template>
 
 <script>
 
 // import Container from './components/Container.vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
 export default {
   name: 'App',
-
+components:{
+  FontAwesomeIcon
+}
 }
 </script>
 
 <style>
 ::-webkit-scrollbar{
   display: none
-}
-body>#app {
-  width: 400px;
-  height:800px;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin: 60px;
-  border:solid 1px black;
-  /* padding:10px; */
-
 }
 
 body,div{
@@ -37,5 +43,42 @@ body,div{
 
 body{
   margin:0;
+}
+.app-container{
+  width: 400px;
+  height:900px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 60px;
+  border:solid 1px black;
+  position: relative;
+  /* padding:10px; */
+
+}
+.router-container{
+  position:absolute;
+  top:50px;
+  overflow-y:scroll;
+}
+
+.status-bar, .tab-bar{
+  display: flex;
+  width:100%;
+  position:absolute;
+  height:50px;
+
+}
+.status-bar{
+  top:0;
+  padding-top: 10px;
+  padding-right: 20px;
+  flex-direction: row-reverse;
+}
+.tab-bar{
+  bottom:0;
+  justify-content: space-evenly;
 }
 </style>
--- frontend/src/components/Feed.vue
+++ frontend/src/components/Feed.vue
@@ -1,23 +1,10 @@
 <template>
 <div class="feed">
-  <div class="status-bar">
-      <!-- <router-link to="/login">Login</router-link> -->
-      <span><font-awesome-icon :icon="['fa', 'paper-plane']" /></span>
-    </div>
-
     <div class="feed-container">
         <div v-for="(item,i) in postdata" :key="i">
             <Post :post="item"/>
         </div>
     </div>
-    <div class="tab-bar">
-      <span><font-awesome-icon :icon="['fa', 'home']"/></span>
-      <span><font-awesome-icon :icon="['fa', 'search']"/></span>
-      <router-link to="/write"><span><font-awesome-icon :icon="['fa', 'plus-square']" /></span> </router-link>
-      <span><font-awesome-icon :icon="['fa', 'heart']" /></span>
-      <span><font-awesome-icon :icon="['fa', 'user-circle']" /></span>
-    </div>
-
 </div>
     
 
@@ -26,7 +13,7 @@
 <script>
 import axios from 'axios'
 import Post from './Post.vue'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 // import { byPrefixAndName } from '@awesome.me/kit-KIT_CODE/icons'
 export default {
     name: 'Feed',
@@ -46,7 +33,7 @@ export default {
     },
     components:{
         Post,
-        FontAwesomeIcon
+        // FontAwesomeIcon
     },
     props:{
 
@@ -56,32 +43,6 @@ export default {
 
 <style>
 
-.feed{
-  position:relative;
-  height:800px;
-}
-.status-bar, .tab-bar{
-  display: flex;
-  position:absolute;
-  width:100%;
-  height:50px;
 
-}
-.status-bar{
-  top:0;
-  padding-top: 10px;
-  padding-right: 20px;
-  flex-direction: row-reverse;
-}
-.tab-bar{
-  bottom:0;
-  justify-content: space-evenly;
-}
 
-.feed-container{
-  position:absolute;
-  top:50px;
-  height:700px;
-  overflow-y:scroll;
-}
 </style>
