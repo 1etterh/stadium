9a0b34fed728dc25a00f84c848a2adffa9f43c0d
1etterh <1etterh.dev@gmail.com>
2024-04-23 14:58:38 +0900

    change: feed.js to get feed rows from postview
    get post rows from postview

--- backend/routes/feed.js
+++ backend/routes/feed.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const connection= require('../database')
-// const postdata=require('./../postdata.json')
-// const userdata=require('./../userdata.json')
+
 /* GET feed  */
-// connection.connect();
-const joinQuery = 'SELECT username, profileImage, contents, image, datetime FROM credential.user t1 Join post.post t2 on t2.userId=t1.userId'
+
+
+const joinQuery = 'SELECT * FROM post.postview'
 router.get('/',async(req, res, next)=>{
   console.log('/routes/feed.js')
   await connection.query(joinQuery,(err,rows,fields)=>{
