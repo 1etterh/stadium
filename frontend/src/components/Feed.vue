4515a035aa36a4b418ef34f0da99cdc20c037dbb
1etterh <1etterh.dev@gmail.com>
2024-04-23 15:59:14 +0900

    fix: nested div boxs bug

--- frontend/src/components/Feed.vue
+++ frontend/src/components/Feed.vue
@@ -1,28 +1,33 @@
 <template>
- <div class="feed">
-    <div class="status-bar">
-    <p>Home</p>
-    <p>Message</p>
-    <router-link to="/login">Login</router-link>
-  </div>
-  <div class="feed-container">
-    <div v-for="(item,i) in postdata" :key="i">
-        <Post :post="item"/>
-    </div>
+<div class="feed">
+  <div class="status-bar">
+      <p>Home</p>
+      <p>Message</p>
+      <router-link to="/login">Login</router-link>
   </div>
 
+    <div class="feed-container">
+        <div v-for="(item,i) in postdata" :key="i">
+            <Post :post="item"/>
+        </div>
+    </div>
     <div class="tab-bar">
-    <span>Feed</span>
-    <span>Search</span>
-    <router-link to="/write">Write</router-link>
-    <span>MyPage</span>
-  </div>
+      <span>Feed</span>
+      <span><font-awesome-icon :icon="['fas', 'search']" /></span>
+      
+      <span>MyPage</span>
+    </div>
+
 </div>
+    
+
 </template>
 
 <script>
 import axios from 'axios'
 import Post from './Post.vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+// import { byPrefixAndName } from '@awesome.me/kit-KIT_CODE/icons'
 export default {
     name: 'Feed',
     created(){
@@ -40,7 +45,8 @@ export default {
         }
     },
     components:{
-        Post
+        Post,
+        FontAwesomeIcon
     },
     props:{
 
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,4 +3,14 @@ import App from './App.vue'
 import router from './routes/index.js'
 import "vue-picocss/css/pico.min.css"
 // import axios from 'axios'
-createApp(App).use(router).mount('#app') 
+
+/* import font awesome icon component */
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(fas)
+
+
+const app= createApp(App).use(router).mount('#app') 
+app.component('font-awesome-icon', FontAwesomeIcon)
