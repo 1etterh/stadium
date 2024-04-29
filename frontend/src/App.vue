2fdcc5b58905841f95ed4af788b2156d74bb4c07
1etterh <1etterh.dev@gmail.com>
2024-04-29 12:09:36 +0900

    backup: stadium

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,33 +1,34 @@
 <template>
-  <div class="app-container">
-  <div class="status-bar">
-      <!-- <router-link to="/login">Login</router-link> -->
-      <span><font-awesome-icon :icon="['fa', 'paper-plane']" /></span>
-    </div>
-
-    <div class="router-container">
-      <router-view></router-view>
-    </div>
-    <div class="tab-bar">
-      <span><font-awesome-icon :icon="['fa', 'home']"/></span>
-      <span><font-awesome-icon :icon="['fa', 'search']"/></span>
-      <router-link to="/write"><span><font-awesome-icon :icon="['fa', 'plus-square']" /></span> </router-link>
-      <span><font-awesome-icon :icon="['fa', 'heart']" /></span>
-      <span><font-awesome-icon :icon="['fa', 'user-circle']" /></span>
-    </div>
-
-  </div>
+<nav>
+  <ul>
+    <li><strong>Acme Corp</strong></li>
+  </ul>
+  <ul>
+    <li><a href="#" class="secondary">Services</a></li>
+    <li>
+      <details class="dropdown">
+        <summary>
+          Account
+        </summary>
+        <ul dir="rtl">
+          <li><a href="#">Profile</a></li>
+          <li><a href="#">Settings</a></li>
+          <li><a href="#">Security</a></li>
+          <li><a href="#">Logout</a></li>
+        </ul>
+      </details>
+    </li>
+  </ul>
+</nav>
 </template>
 
 <script>
 
-// import Container from './components/Container.vue'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 export default {
   name: 'App',
 components:{
-  FontAwesomeIcon
+  
 }
 }
 </script>
@@ -44,41 +45,5 @@ body,div{
 body{
   margin:0;
 }
-.app-container{
-  width: 400px;
-  height:900px;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin: 60px;
-  border:solid 1px black;
-  position: relative;
-  /* padding:10px; */
 
-}
-.router-container{
-  position:absolute;
-  top:50px;
-  overflow-y:scroll;
-}
-
-.status-bar, .tab-bar{
-  display: flex;
-  width:100%;
-  position:absolute;
-  height:50px;
-
-}
-.status-bar{
-  top:0;
-  padding-top: 10px;
-  padding-right: 20px;
-  flex-direction: row-reverse;
-}
-.tab-bar{
-  bottom:0;
-  justify-content: space-evenly;
-}
 </style>
